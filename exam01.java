0fafbc2097af0a6c74a98ddb7a6e912c8d190764
seul <tpalbok@naver.com>
2021-01-20 15:06:06 +0900

    -

---
+++ exam01.java
@@ -0,0 +1,14 @@
+package day0120;
+
+public class exam01 {
+	 
+
+	public static void main(String[] args) {
+		String user = "Chung seul-gi";
+		
+				System.out.println("user: " + user);
+		// 
+
+	}
+
+}
